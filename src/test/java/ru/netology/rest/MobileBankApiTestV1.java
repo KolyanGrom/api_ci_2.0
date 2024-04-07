e596163edcd062ea979a2b17e0dda1d548458343
KolyanGrom <kolyangrom@yandex.ru>
2024-04-07 21:36:43 +0700

    reformate code

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -10,19 +10,19 @@ import static org.hamcrest.Matchers.hasSize;
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-              .header("Content-Type", "application/json; charset=UTF-8")
-              .contentType(ContentType.JSON)
-              .body("", hasSize(3));
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3));
     }
 }
